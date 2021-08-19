6a76b46bc4f08cbca3a9e970bb197fedba3406f6
Radmacher <git@radmacher.me>
2021-08-19 20:15:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hori</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+</style>
