fbc37110f133c5a51520010fab69d137fdc2ed3d
Radmacher <git@radmacher.me>
2021-08-20 19:08:24 +0200

    Added Vue router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
-  <router-view></router-view>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App',
+  name: 'Home',
 });
 </script>
 
-<style>
+<style scoped>
+
 </style>
