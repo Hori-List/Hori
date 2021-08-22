853cc44df7ee75b382e6e2cd2be5c46727149f67
Radmacher <git@radmacher.me>
2021-08-22 13:48:29 +0200

    Added Appwrite methods

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -1,12 +1,46 @@
-import {Appwrite} from 'appwrite';
+import { Appwrite } from 'appwrite';
 
 const appwrite = new Appwrite();
 
-const endpoint = import.meta.env.APP_ENDPOINT as string;
-const project = import.meta.env.APP_PROJECT as string;
+const endpoint = import.meta.env.VITE_APP_ENDPOINT as string;
+const project = import.meta.env.VITE_APP_PROJECT as string;
+
+const listsCollection = import.meta.env.VITE_APP_LISTS_COLLECTION as string;
 
 appwrite.setEndpoint(endpoint).setProject(project);
 
-export {
-  appwrite,
+async function createAccount(email: string, password: string, name: string) {
+  return await appwrite.account.create(email, password, name).then((user) => {
+    return [user, null];
+  }, (err) => {
+    return [null, err];
+  });
 }
+
+async function login(email: string, password: string) {
+  return await appwrite.account.createSession(email, password).then((user) => {
+    return [user, null];
+  }, (err) => {
+    return [null, err];
+  });
+}
+
+async function logout() {
+  return await appwrite.account.deleteSession('current');
+}
+
+async function createList(name: string) {
+  const data = {
+    name,
+  };
+  return await appwrite.database.createDocument(listsCollection, data).then((res) => {
+    return [res, null];
+  }, (err) => {
+    return [null, err];
+  });
+}
+
+
+export {
+  appwrite, createList, login, logout, createAccount,
+};
