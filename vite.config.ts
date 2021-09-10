dced0ad2e1b5d05be51a932fd1d6ce35d519c22c
Radmacher <git@radmacher.me>
2021-09-10 22:21:05 +0200

    Implemented icons and icon auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
+import Components from 'vite-plugin-components';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(), WindiCSS(), Components({
+      customComponentResolvers: IconsResolver(),
+    }), Icons(),
+  ],
 });
