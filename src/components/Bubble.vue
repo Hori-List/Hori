d37905838c82a5ea26489c2c4d9ca9e84ef29317
Radmacher <git@radmacher.me>
2021-09-10 22:18:48 +0200

    Added Bubble component to display one digit numbers

---
+++ src/components/Bubble.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bubble">
+    <p class="number">{{ number }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Bubble',
+  props: {
+    number: Number,
+  },
+});
+</script>
+
+<style scoped>
+.bubble {
+  @apply w-5 h-5 rounded-full dark:bg-white;
+}
+
+.number {
+  @apply text text-sm font-regular text-center dark:text-black;
+}
+</style>
