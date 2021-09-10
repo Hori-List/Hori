c4734d2ada5dad8000a761054817d69b152c239e
Radmacher <git@radmacher.me>
2021-09-10 22:22:39 +0200

    Added Seperator component to group items in a list

---
+++ src/components/Seperator.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-screen flex items-center px-6">
+    <div class="w-1/8 line"></div>
+    <p class="seperator-text">{{ text }}</p>
+    <div class="w-full line"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Seperator',
+  props: {
+    text: String,
+  },
+});
+</script>
+
+<style scoped>
+.line {
+  @apply h-px dark:bg-white;
+}
+
+.seperator-text {
+  @apply text font-normal text-base mx-3 min-w-max;
+}
+</style>
