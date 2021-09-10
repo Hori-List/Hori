e501feb562f9da7568d817e4ee111bcf4238d4b8
Radmacher <git@radmacher.me>
2021-09-10 22:26:20 +0200

    Implemented method to manage theme and the initial load of user data

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,32 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia'
 import 'virtual:windi.css';
+import { useStore } from './store';
+import { getInvitations, getLists, getUser } from './appwrite';
 
 createApp(App).use(router).use(createPinia()).mount('#app');
+
+async function home() {
+  // set theme
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+
+  const store = useStore();
+
+  const user = await getUser();
+  if (user.id) {
+    await store.$patch({ user });
+    const lists = await getLists();
+    const invitations = await getInvitations();
+    store.$patch({ lists, invitations });
+    router.push('/lists');
+  } else {
+    router.push('/');
+  }
+}
+
+home();
